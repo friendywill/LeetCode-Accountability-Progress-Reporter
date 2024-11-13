9d6caa3cfed7d4e614e08319de3edca6c0d93890
friendywill <will@friendy.dev>
2024-11-14 07:02:43 +1100

    Added CRON_INTERVAL for script.

--- run.sh
+++ run.sh
@@ -1,1 +1,14 @@
-node ./accountable.js >> ./accountable.log
+#!/bin/sh
+# run.sh
+
+touch ./accountable.log
+
+# Loop indefinitely
+while true
+do
+    # Run the script
+    node ./accountable.js >> ./accountable.log
+
+    # Wait for the CRON_INTERVAL
+    sleep ${CRON_INTERVAL}
+done
